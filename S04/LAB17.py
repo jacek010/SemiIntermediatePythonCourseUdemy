cd3bf902b775b5e753605a8a292e489a376c9bc5
jacek010 <jmyjkowski@gmail.com>
2024-02-06 18:39:51 +0100

    LAB17 - function as variable

---
+++ S04/LAB17.py
@@ -0,0 +1,27 @@
+# Funkcja jako zmienna
+
+def double(x):
+    return 2 * x
+
+
+def square(x):
+    return x ** 2
+
+
+def negative(x):
+    return -x
+
+
+def div2(x):
+    return x / 2
+
+
+number = 8
+
+transformations = [double, square, div2, negative]
+tmp_return_value = number
+
+for transformation in transformations:
+    tmp_return_value = transformation(tmp_return_value)
+    print(transformation.__name__, tmp_return_value)
+
