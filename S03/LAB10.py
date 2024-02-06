24318f943558571ddde2ea65116e3e06b24a48e6
jacek010 <jmyjkowski@gmail.com>
2024-02-06 14:59:31 +0100

    LAB10 - Zagnieżdżanie pętli i to samo w postaci jednolinijkowej

---
+++ S03/LAB10.py
@@ -0,0 +1,17 @@
+ports = ['WAW', 'KRK', 'GDN', 'KTW', 'WMI', 'WRO', 'POZ', 'RZE', 'SZZ',
+         'LUZ', 'BZG', 'LCJ', 'SZY', 'IEG', 'RDO']
+
+
+connections = [(start, end) for start in ports for end in ports]
+print("All connections", len(connections))
+print(connections)
+
+
+connections_no_self = [(start, end) for start in ports for end in ports if start != end]
+print("All connections without self-connections", len(connections_no_self))
+print(connections_no_self)
+
+
+routes = [(start, end) for start in ports for end in ports if start < end]
+print("All routes without self-connections and doubled connections", len(routes))
+print(routes)
