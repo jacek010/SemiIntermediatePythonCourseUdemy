14cda5ae45f8eddb8c6f591e783f476cace7849e
jacek010 <jmyjkowski@gmail.com>
2024-02-12 13:26:50 +0100

    LAB42 - multiple error handling

---
+++ S08/LAB42.py
@@ -0,0 +1,52 @@
+import requests
+import os
+import shutil
+
+
+def save_url_to_file(url, file_path):
+
+    r = requests.get(url, stream = True)
+    with open(file_path, "wb") as f:
+        f.write(r.content)
+
+
+url = 'http://www.mobilo24.eu/spis/'
+dir = 'LAB41_files'
+tmpfile = 'download.tmp'
+file = 'spis.html'
+
+tmpfile_path = os.path.join(dir, tmpfile)
+file_path = os.path.join(dir, file)
+
+try:
+    if os.path.exists(tmpfile_path):
+        print(f'Removing {tmpfile_path}')
+        os.remove(tmpfile_path)
+
+    print(f'Downloading {url}')
+    save_url_to_file(url, tmpfile_path)
+
+    print(f'Copying {tmpfile_path} to {file_path}')
+    shutil.copy(tmpfile_path, file_path)
+
+except requests.exceptions.ConnectionError as e:
+    print(f'Could not connect to {url} | Details: {e}')
+
+except PermissionError as e:
+    print(f'Could not save to file | Details: {e}')
+
+except FileNotFoundError as e:
+    print(f'Could not find {tmpfile_path} | Details: {e}')
+
+except Exception as e:
+    print(f'Error occurred')
+    print(f'Error details: {e}')
+
+else:
+    print(f'Successfully downloaded to {file}')
+
+finally:
+    if os.path.exists(tmpfile_path):
+        print(f'Final removal of {tmpfile_path}')
+        os.remove(tmpfile_path)
+    print('DONE')
